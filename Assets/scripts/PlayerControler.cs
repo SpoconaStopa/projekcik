c4357f9d6f9b2e8059462ed1d8c7b9c43d04b16a
SpoconaStopa <kacperjanas2008@gmail.com>
2025-05-14 20:13:37 +0200

    Adding assets

--- Assets/scripts/PlayerControler.cs
+++ Assets/scripts/PlayerControler.cs
@@ -3,40 +3,70 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
-
 public class PlayerController : MonoBehaviour
 {
-    // Zmienne wykorzystywane do ruchu
-    public InputAction MoveAction; 
-    public int speed;
-    Rigidbody2D rigidbody2d;
-    Vector2 move;
-    SpriteRenderer m_SpriteRenderer;
+    // Mo¿liwe stany gracza
+    private enum PlayerState { Idle, Moving, Dash }
+    private PlayerState playerState;
+
+    // ===== Publiczne ustawienia =====
+    public InputAction MoveAction;    // Akcja wejœcia z Input Systemu
+    public int speed;                 // Prêdkoœæ poruszania
+    public int dash_value;           // Prêdkoœæ lub wartoœæ dashu (niewykorzystana obecnie)
+    public int dash_time;            // Czas trwania dashu
+    public int dash_time_cooldown;   // Czas trwania cooldown dashu
+
+    // ===== Prywatne komponenty =====
+    private Rigidbody2D rigidbody2d;     // Komponent fizyki
+    private SpriteRenderer m_SpriteRenderer; // Do odbicia sprite'a
+    private Animator animation;          // Animator (s³owo "animation" nie jest zarezerwowane, ale lepiej go nie nadu¿ywaæ)
+    private float dash;                  //Timer odliczaj¹cy do koñca dash
+    private float dash_cooldown;         //Timer odliczaj¹cy do koñca cooldown dash
+
+    // ===== Dane wejœciowe =====
+    private Vector2 move;    // Wartoœæ wejœcia z klawiatury / kontrolera
 
     void Start()
     {
+        // W³¹czenie akcji ruchu
         MoveAction.Enable();
+
+        // Pobranie komponentów z obiektu gracza
         rigidbody2d = GetComponent<Rigidbody2D>();
         m_SpriteRenderer = GetComponent<SpriteRenderer>();
+        animation = GetComponent<Animator>();
     }
 
     void Update()
     {
+        // Odczyt wektora ruchu z Input Systemu
         move = MoveAction.ReadValue<Vector2>();
-        if(move.x < 0)
-        {
-            m_SpriteRenderer.flipX = true;
-        }
-        if (move.x > 0)
-        {
-            m_SpriteRenderer.flipX = false;
-        }
+        Debug.Log(move);
+            switch (playerState)
+            {
+                case PlayerState.Idle:
+                    if (move.x < 0 || move.y < 0)
+                    {
+                    playerState = PlayerState.Moving; 
+                    break;
+                    }
+                    break;
 
+                case PlayerState.Moving:
+                    
+                    break;
+
+                case PlayerState.Dash: 
+                    break;
+            }
     }
 
     void FixedUpdate()
     {
+        // Oblicz now¹ pozycjê w zale¿noœci od ruchu, prêdkoœci i deltaTime
         Vector2 position = (Vector2)rigidbody2d.position + move * speed * Time.fixedDeltaTime;
+
+        // Przesuñ gracza do nowej pozycji z u¿yciem fizyki
         rigidbody2d.MovePosition(position);
     }
 }
